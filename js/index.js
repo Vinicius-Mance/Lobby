dc4439d9edd4596bfd84d65f8686cdddb24c1e53
Vinicius-Mance <vinimanceprof@gmail.com>
2021-10-12 19:37:44 -0300

    links adicionados

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 let music = document.getElementById("music");
 let player = document.getElementById("player");
 let muted = document.getElementById("muted");
-
+music.volume = 0.3;
 player.onclick = function () { toggleMusic(); }
 muted.onclick = function () { toggleMusic(); }
 
@@ -21,16 +21,16 @@ const toggleMusic = () => {
 
 }
 
-var vol = 1;
-var interval = 30; // 200ms interval
+var vol = 0.3;
+var interval = 30;
 
 const fadeOut = () => {
   var fade = setInterval(
     function() {
       if (playing) {
         return;
-      } else if (vol > 1) {
-        vol = 1;
+      } else if (vol > 0.3) {
+        vol = 0.3;
       } else if (vol > 0.2) {
         music.volume = vol;
         vol -= 0.05;
@@ -52,10 +52,10 @@ const fadeIn = () => {
         return;
       } else if (vol < 0 ) {
         vol = 0;
-      } else if (vol < 0.2) {
+      } else if (vol == 0) {
         music.volume = vol;
         vol += 0.01;
-      } else if (vol < 1) {
+      } else if (vol < 0.3) {
         music.volume = vol;
         vol += 0.05;
       } else {
